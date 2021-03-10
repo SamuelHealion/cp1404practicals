7b3ff15a1b0d1941ee76f6d69ed91dc1e86499fa
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-03-10 19:24:41 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -4,14 +4,27 @@ It will then print the length of the valid password in *'s
 """
 
 MIN_LENGTH = 5
-valid_password = False
 
-while not valid_password:
-    password = str(input("Please enter a password at least {} characters long: ".format(MIN_LENGTH)))
-    if len(password) < MIN_LENGTH:
-        print("That password isn't long enough")
-    else:
-        print('*' * len(password))
-        # for char in password:
-        #     print('*', end='')
-        valid_password = True
+
+def main():
+    password = get_password()
+    print_stars(password)
+
+
+def get_password():
+    """Checks whether a password is longer than the MIN_LENGTH required."""
+    valid_password = False
+    while not valid_password:
+        password = str(input("Please enter a password at least {} characters long: ".format(MIN_LENGTH)))
+        if len(password) < MIN_LENGTH:
+            print("That password isn't long enough")
+        else:
+            return password
+
+
+def print_stars(string):
+    """Prints * for the length of the string passed."""
+    print('*' * len(string))
+
+
+main()
