10b3de0fc7789c5aafbcb5326b5cb3775a5ff1bd
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-03-10 19:57:06 +1000

    Refactor broken_score.py and temperature.py to use functions.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,14 +1,32 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
+Returns a result based on a users score out of 100
+Refactored to use functions
 """
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-elif score < 50:
-    print("Bad")
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    result = score_result(score)
+    print(result)
+
+    random_score = random.randrange(0, 100)
+    result = score_result(random_score)
+    print("Random score is: {}".format(random_score))
+    print(result)
+
+
+def score_result(score):
+    if score > 100 or score < 0:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
