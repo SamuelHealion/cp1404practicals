ef243f299cb536416ca24c8691e7bb50e5f018ee
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-03-16 16:32:20 +1000

    Add lists_warmup and total_income code for initial commit

---
+++ prac_4/lists_warmup.py
@@ -0,0 +1,22 @@
+"""
+Practice with lists to establish an understanding of their uses and how to manipulate them
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0]                3
+# numbers[-1]               2
+# numbers[3]                1
+# numbers[:-1]              3, 1, 4, 1, 5, 9
+# numbers[3:4]              1
+# 5 in numbers              true
+# 7 in numbers              false
+# "3" in numbers            false
+# numbers + [6, 5, 3]       3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+
+numbers[0] = "10"
+print(numbers)
+numbers[-1] = 1
+print(numbers)
+print(numbers[2:])
+print(9 in numbers)
