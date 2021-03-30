8fa21bd5d5e11706bfd71cbd57c521930b3f52db
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-03-31 09:07:29 +1000

    Fix word_occurrences program based on feedback

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,22 +5,22 @@ Takes a sentence from the user and counts how many times a word appears using di
 
 
 def main():
-    """Take user input and count how many times a word appears."""
-    user_text = str(input("Text: "))
-    split_words = user_text.split()
-    all_words = {}
-    for word in split_words:
-        if word in all_words:
-            all_words[word] += 1
+    """Take user input and count how many times a word occurs."""
+    words_to_occurrences = {}
+    user_text = input("Text: ")
+    user_words = user_text.split()
+    for word in user_words:
+        if word in words_to_occurrences:
+            words_to_occurrences[word] += 1
         else:
-            all_words[word] = 1
+            words_to_occurrences[word] = 1
 
-    sorted_words = list(all_words.keys())
-    sorted_words.sort()
-    longest_word = max((len(word) for word in sorted_words))
+    words = list(words_to_occurrences.keys())
+    words.sort()
+    longest_word = max((len(word) for word in words))
 
-    for word in sorted_words:
-        print("{:{}} : {}".format(word, longest_word, all_words[word]))
+    for word in words:
+        print("{:{}} : {}".format(word, longest_word, words_to_occurrences[word]))
 
 
 main()
