81ee609fc12537d01efc6d8c9f75265ee43c7f18
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-03-30 20:13:31 +1000

    Add languages program with programming_language class program

---
+++ prac_06/languages.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Practical 6 - Classes
+Uses classes to define programming languages and then checks if they are dynamically typed
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+languages = [ruby, python, visual_basic]
+print("The dynamically typed languages are:")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
---
+++ prac_06/programming_language.py
@@ -0,0 +1,28 @@
+"""
+CP1404 Practical 6 - Classes
+Define the class ProgrammingLanguage
+"""
+
+
+class ProgrammingLanguage:
+    """Represents a Programming Language object."""
+
+    def __init__(self, name='', typing='', reflection=bool, year=0):
+        """Initialize a Programming Language instance."""
+
+        self.name = name
+        self.typing = typing.title()
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Returns whether a car is Dynamic or not."""
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        """Returns a string representation of a Programming Language."""
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing,
+                                                                           self.reflection, self.year)
