642f6bd9b84f133b8e216303915cf42dc826db36
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-03-31 08:43:39 +1000

    Fix emails program based on feedback

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,28 +1,29 @@
 """
-CP1404 Practical 5 Stores a users email and name in a dictionary. It will attempt to extract
+CP1404 Practical 5
+Stores a users email and name in a dictionary. It will attempt to extract
 the name of the user based off the email address
 """
 
 
 def main():
-    """Creates a dictionary of user emails and names"""
+    """Create a dictionary of user emails and names."""
     saved_emails = {}
-    email = str(input("Email: "))
+    email = input("Email: ")
     while email != '':
         name = get_name(email)
-        correct_name = str(input("Is this name correct? {} (Y/n) ".format(name)))
-        if correct_name.lower() != 'y' and correct_name != '':
-            name = str(input("Name: ")).title()
+        correct_name = input("Is this name correct? {} (Y/n) ".format(name))
+        if correct_name.upper() != 'Y' and correct_name != '':
+            name = input("Name: ").title()
 
-        saved_emails[email] = name
-        email = str(input("Email: "))
+        saved_emails[name] = email
+        email = input("Email: ")
 
     for name, email in saved_emails.items():
-        print("{} ({})".format(email, name))
+        print("{} ({})".format(name, email))
 
 
 def get_name(email):
-    """Gets a name from an email address."""
+    """Get a name from an email address."""
     name = email.split('@')
     split_name = name[0].split('.')
     full_name = ' '.join(split_name).title()
