7285e8079aa4e36e46b0bd881f7ef6756837ef45
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-04-10 14:50:21 +1000

    Add Guitar Class program and its test program

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,14 @@
+"""
+CP1404 Practical 6 - Classes
+Short program to check that the Guitar class works
+"""
+
+from prac_06.guitar import Guitar
+
+gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+another_guitar = Guitar("Other Guitar", 2013, 400)
+
+print("{} get_age() - Expected 99. Got {}".format(gibson.name, gibson.get_age()))
+print("{} get_age() - Expected 8. Got {}".format(another_guitar.name, another_guitar.get_age()))
+print("{} is_vintage() - Expected True. Got {}".format(gibson.name, gibson.is_vintage()))
+print("{} is_vintage() - Expected False. Got {}".format(another_guitar.name, another_guitar.is_vintage()))
