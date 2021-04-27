8170a58a91e4b0eb240576d1bd0994e6e0e31224
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-04-27 15:36:08 +1000

    Add UnreliableCar class and the program to test it

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Practical 8 - Inheritance
+A copy of the car class from the practical in week 6.
+"""
+
+from unreliable_car import UnreliableCar
+
+
+def main():
+    lemon = UnreliableCar('Bessy', 100, 50)
+    print(lemon)
+    lemon.drive(40)
+    print(lemon)
+
+
+main()
