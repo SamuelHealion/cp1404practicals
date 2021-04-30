0ec850d1df24120111efcd483ed9c62a16b08d84
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-04-30 12:41:41 +1000

    Add taxi_simulator program

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,76 @@
+"""
+CP1404 Practical 8 - Inheritance
+Program to simulate taxi's using the Taxi and SilverServiceTaxi classes.
+"""
+
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    """Simulate taxi service using Taxi and SilverServiceTaxi Classes."""
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    bill_to_date = 0
+
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ")
+    while choice.lower() != 'q':
+        if choice.lower() == 'c':
+            print("Taxis available:")
+            taxis_available(taxis)
+            current_taxi = get_valid_taxi(taxis)
+        elif choice.lower() == 'd':
+            if current_taxi:    # Check that the is a currently selected taxi
+                distance = get_valid_distance()
+                if current_taxi.fuel == 0:
+                    print("Sorry, that taxi is all out of fuel. Please choose another")
+                else:
+                    current_taxi.start_fare()
+                    current_taxi.drive(distance)
+                    trip_fare = current_taxi.get_fare()
+                    print("Your {} trip cost you ${:.2f}".format(current_taxi.name, trip_fare))
+                    bill_to_date += trip_fare
+            else:
+                print("No taxi selected, please choose a taxi first")
+        else:
+            print("Invalid choice")
+        print("Bill to date: ${:.2f}".format(bill_to_date))
+        print(MENU)
+        choice = input(">>> ")
+    print("Taxis are now:")
+    taxis_available(taxis)
+
+
+def taxis_available(taxis):
+    """Display the details of the taxis available."""
+    for i, car in enumerate(taxis):
+        print("{} - {}".format(i, car))
+
+
+def get_valid_distance():
+    """Get a valid input for distance."""
+    try:
+        distance = int(input("Drive how far? "))
+    except ValueError:
+        print("Invalid distance")
+        distance = 0
+    return distance
+
+
+def get_valid_taxi(taxis):
+    """Get a valid choice for current taxi."""
+    try:
+        choice = int(input("Choose taxi: "))
+        if choice < 0 or choice >= len(taxis):
+            print("Invalid choice")
+        else:
+            return taxis[choice]
+    except ValueError:
+        print("Invalid choice")
+
+
+main()
